e0feaed86ee04a7116a4ee18eb2bac9af1976913
Asayake <amoqm88uk@gmail.com>
2019-06-23 15:22:12 +0100

    Basic portal layout set up.

--- public/portalhome.js
+++ public/portalhome.js
@@ -2,13 +2,40 @@
     //Set up page - hide inactive sections
     $('#view-chart-content').hide();
 
-    //Stub out basic btn functionality
+
+    //Top menu button actions:
     $('#view-map-btn').click((event)=>{
-        alert('clicked map btn');
+        $('#view-chart-content').hide();
+        $('#view-map-content').show();
+        //update styling: //TODO: make class removal general
+        $('#view-chart-btn').removeClass('active');
+        $('#view-map-btn').removeClass('active');
+        $('#view-map-btn').addClass('active');
     });
 
     $('#view-chart-btn').click((event)=>{
-        alert('clicked chart btn');
+        $('#view-map-content').hide();
+        $('#view-chart-content').show();
+        //update styling: //TODO: make class removal general
+        $('#view-map-btn').removeClass('active');
+        $('#view-chart-btn').removeClass('active');
+        $('#view-chart-btn').addClass('active');
     });
+
+    //Map Options button actions:
+    $('#map-show-actions').click((event)=>{
+        //update styling:
+        $('#map-show-reasons').removeClass('active');
+        $('#map-show-actions').removeClass('active');
+        $('#map-show-actions').addClass('active');
+    });
+
+    $('#map-show-reasons').click((event)=>{
+        //update styling:
+        $('#map-show-actions').removeClass('active');
+        $('#map-show-reasons').removeClass('active');
+        $('#map-show-reasons').addClass('active');
+    });
+
 
   });
